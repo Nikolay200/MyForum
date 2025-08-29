b4d5b3d878294842faac6613f4a91a72530abec2
Nikolay Smirnov <nikolay.smirnov@autogramma.ru>
2025-08-29 09:41:12 +0300

    Add infrastructure for Topic

---
+++ Domain/Abstractions/Entity.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Domain.Abstractions
+{
+    public abstract class Entity<T> : IEntity<T>
+    {
+        public required T Id { get; set; }
+    }
+}
---
+++ Domain/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Domain.Exceptions
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string message):base ($"Domain exception: ({message}).") 
+        {
+        
+        }
+    }
+}
