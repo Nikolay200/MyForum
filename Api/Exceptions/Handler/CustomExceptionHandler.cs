4338b183d34f03958d8380cb5ccd73ec90e7718b
Nikolay Smirnov <nikolay.smirnov@autogramma.ru>
2025-09-08 11:54:29 +0300

    Fill CustomExceptionHandler

--- Api/Exceptions/Handler/CustomExceptionHandler.cs
+++ Api/Exceptions/Handler/CustomExceptionHandler.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Diagnostics;
+﻿using Application.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
 
 namespace Api.Exceptions.Handler
 {
-    public class CustomExceptionHandler : IExceptionHandler
+    public class CustomExceptionHandler(ILogger<CustomExceptionHandler> logger) : IExceptionHandler
     {
         public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            logger.LogWarning("Обработанное исключение: {Message}, время: {time}", exception.Message, DateTime.Now);
+
+            (string Detail, string Title, int StatusCode) details = exception switch
+            {
+                NotFoundException =>(
+                exception.Message, 
+                exception.GetType().Name, 
+                httpContext.Response.StatusCode = StatusCodes.Status404NotFound
+                ),
+                _=>(
+                exception.Message, 
+                exception.GetType().Name,
+                httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError
+                )
+            };
+
+            var problemDetails = new Microsoft.AspNetCore.Mvc.ProblemDetails 
+            { 
+                Title = details.Title,
+                Detail = details.Detail,
+                Status = details.StatusCode,
+                Instance = httpContext.
+            };
+
         }
     }
 }
